be26dea11620d9ab641af7c6c1d17d293ed93271
Danil Sereda <seredadanil944@gmail.com>
2024-09-07 15:58:47 +0300

    07.09.2024

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
 from firstapp import views
-
 urlpatterns = [
-    path('',views.index,name='home'),
-    path('admin/', admin.site.urls),
+re_path (r'^about/contact/', views.contact),
+re_path(r'^about', views.about),
+path ("", views. index),
 ]
 
