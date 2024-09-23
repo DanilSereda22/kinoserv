a0b3ddba438484656f099debb133ea5c5027e172
Danil Sereda <seredadanil944@gmail.com>
2024-09-23 16:30:10 +0300

    23.09.2024 Sereda Danil научился пользоваться параметрами представлений.

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -1,9 +1,14 @@
+from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
 from firstapp import views
 urlpatterns = [
-re_path (r'^about/contact/', views.contact),
-re_path(r'^about', views.about),
-path ("", views. index),
+ path('', views.index),
+ re_path(r'^about', views.about),
+ re_path(r'^contact', views.contact),
+ path('products/', views.products), 
+ path('products/<int:productid>/', views.products),
+ path('users/', views.users), 
+ path('users/<int:id>/<str:name>/', views.users),
 ]
 
